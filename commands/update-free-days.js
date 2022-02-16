7499c6f746a0c3b432313df41de1b03cae22b633
gabitodev <gabitodev@gmail.com>
2022-02-16 17:02:15 -0400

    command permissions and name changed

--- commands/update-free-days.js
+++ commands/update-free-days.js
@@ -1,20 +1,22 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { none } = require('../database');
 
-const assignFreeDays = async (interaction) => {
+const updateFreeDays = async (interaction) => {
   // 1. We define the variables
   const teamId = interaction.options.getString('team-id');
   const freeDays = interaction.options.getNumber('free-days');
+
   // 2. We remove the scholar from the database
   await none('UPDATE teams SET free_days = $1 WHERE team_id = $2', [freeDays, teamId]);
+
   // 3. Display the response to the user
-  await interaction.reply(`Assigned ${freeDays} days without fee to team #${teamId}`);
+  await interaction.reply(`Assigned ${freeDays} days without fee to team #${teamId}.`);
 };
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('assign-free-days')
-    .setDescription('Assign days without fee to a team')
+    .setName('update-free-days')
+    .setDescription('Updates days without fee to a team')
     .addStringOption(option =>
       option
         .setName('team-id')
@@ -26,7 +28,7 @@ module.exports = {
         .setDescription('Number of days')
         .setRequired(true)),
   async execute(interaction) {
-    if (interaction.member.roles.cache.has('863179537324048414')) return;
-    await assignFreeDays(interaction);
+    if (interaction.user.id !== interaction.guild.ownerId) return;
+    await updateFreeDays(interaction);
   },
 };
