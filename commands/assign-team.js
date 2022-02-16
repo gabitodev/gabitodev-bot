831a74d3913197d47b4ec06072f735753f364a81
gabitodev <gabitodev@gmail.com>
2022-02-16 19:18:39 -0400

    error reply changed

--- commands/assign-team.js
+++ commands/assign-team.js
@@ -2,10 +2,11 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { result } = require('../database');
 
 const assignTeam = async (interaction) => {
-  // 1. We define the variables
-  const teamId = interaction.options.getNumber('team-id');
-  const discordId = interaction.options.getUser('discord-user').id;
   try {
+    // 1. We define the variables
+    const teamId = interaction.options.getNumber('team-id');
+    const discordId = interaction.options.getUser('discord-user').id;
+
     // 2. We update the database and check if was suscessfull
     const { rowCount } = await result({
       text: 'UPDATE teams SET discord_id = $1 WHERE team_id = $2',
@@ -17,7 +18,7 @@ const assignTeam = async (interaction) => {
     await interaction.reply(`Successfully assigned team #${teamId} to scholar <@${discordId}>.`);
   } catch (error) {
     if (error.code === '23503') {
-      return await interaction.reply(`The team could not be assigned because <@${discordId}> is not a scholar!`);
+      return await interaction.reply('The team could not be assigned because the discord user is not a scholar.');
     } else {
       console.log(error);
     }
