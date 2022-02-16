6a5f23e6776c8a42338b55433dc658693180c0a3
gabitodev <gabitodev@gmail.com>
2022-02-16 19:10:35 -0400

    better errors

--- commands/assign-team.js
+++ commands/assign-team.js
@@ -1,16 +1,27 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { none } = require('../database');
+const { result } = require('../database');
 
 const assignTeam = async (interaction) => {
   // 1. We define the variables
   const teamId = interaction.options.getNumber('team-id');
   const discordId = interaction.options.getUser('discord-user').id;
+  try {
+    // 2. We update the database and check if was suscessfull
+    const { rowCount } = await result({
+      text: 'UPDATE teams SET discord_id = $1 WHERE team_id = $2',
+      values: [discordId, teamId],
+    });
+    if (rowCount === 0) return await interaction.reply('The team could not be assigned because it is not in the database.');
 
-  // 2. We update the databse
-  await none('UPDATE teams SET discord_id = $1 WHERE team_id = $2', [discordId, teamId]);
-
-  // 3. Display the response to the user
-  await interaction.reply(`Successfully assigned team #${teamId} to scholar <@${discordId}>.`);
+    // 3. Display the response to the user
+    await interaction.reply(`Successfully assigned team #${teamId} to scholar <@${discordId}>.`);
+  } catch (error) {
+    if (error.code === '23503') {
+      return await interaction.reply(`The team could not be assigned because <@${discordId}> is not a scholar!`);
+    } else {
+      console.log(error);
+    }
+  }
 };
 
 module.exports = {
