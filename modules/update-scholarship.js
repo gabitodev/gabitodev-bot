52a36adff2084e881787288edaf7fe20fe3152ad
gabitodev <gabitodev@gmail.com>
2022-02-17 11:01:51 -0400

    removed useless function

--- modules/update-scholarship.js
+++ modules/update-scholarship.js
@@ -20,25 +20,21 @@ const getScholars = async () => {
 
 };
 
-const updateDatabase = async () => {
-  // 1. We obtain all the shcolars
-  const scholars = await getScholars();
-
-  // 2. We obtain each ronin address
-  const roninsAddresses = scholars.map(({ teamAddress }) => teamAddress);
-
-  // 3. We obtain the ronin data
-  const roninData = await getRoninData(roninsAddresses);
-
-  // 4. We update each scholar to the database
-  for (const scholarData of scholars) {
-    const teamStats = calcTeamStats(scholarData, roninData);
-    updateScholar(teamStats);
-  }
-};
-
 module.exports = {
-  async updateScholarship(interaction) {
-    await updateDatabase(interaction);
+  async updateScholarship() {
+    // 1. We obtain all the shcolars
+    const scholars = await getScholars();
+
+    // 2. We obtain each ronin address
+    const roninsAddresses = scholars.map(({ teamAddress }) => teamAddress);
+
+    // 3. We obtain the ronin data
+    const roninData = await getRoninData(roninsAddresses);
+
+    // 4. We update each scholar to the database
+    for (const scholarData of scholars) {
+      const teamStats = calcTeamStats(scholarData, roninData);
+      updateScholar(teamStats);
+    }
   },
 };
