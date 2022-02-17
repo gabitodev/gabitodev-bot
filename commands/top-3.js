8098cd91f5c150cc2ad59e509d8cb643c6c51b36
gabitodev <gabitodev@gmail.com>
2022-02-17 11:08:11 -0400

    better error handling and prevented sql injection

--- commands/top-3.js
+++ commands/top-3.js
@@ -3,32 +3,41 @@ const { AsciiTable3 } = require('ascii-table3');
 const { many } = require('../database');
 
 const getTop = async () => {
-  const text = `
-  SELECT scholars.full_name, teams.mmr FROM Scholars
-  INNER JOIN teams
-  ON scholars.discord_id = teams.discord_id
-  ORDER BY mmr DESC
-  LIMIT 3`;
-  const top3 = await many(text);
-  return top3;
+  try {
+    const top3 = await many({
+      text:`
+      SELECT scholars.full_name, teams.mmr FROM Scholars
+      INNER JOIN teams
+      ON scholars.discord_id = teams.discord_id
+      ORDER BY mmr DESC
+      LIMIT 3`,
+    });
+    return top3;
+  } catch (error) {
+    return null;
+  }
 };
 
 const getTop3 = async (interaction) => {
   // 1. We obtain the top 3 of the scholarships
   const top3Scholars = await getTop();
+  if (!top3Scholars) return await interaction.reply('There was an error loading the top 3! Verify that you have scholars.');
+
   // 2. We make an array to display the leaderboard
-  const orderedTop3 = top3Scholars.map(({ fullName, mmr }, index) => {
-    const array = [`${index + 1}`, `${fullName}`, `${mmr}`];
+  const orderedTop3 = top3Scholars.map(({ scholar_name, mmr }, index) => {
+    const array = [index + 1, scholar_name, mmr];
     return array;
   });
+
   // 3. We create the table to display
   const top3Table = new AsciiTable3('Top 3 scholars of the week')
     .setHeading('Rating', 'Name', 'MMR')
     .setAlign(3)
     .addRowMatrix(orderedTop3)
     .setStyle('unicode-single');
+
   // 4. We display the table to the user
-  await interaction.reply({ content: codeBlock(top3Table) });
+  await interaction.reply(codeBlock(top3Table));
 };
 
 module.exports = {
@@ -36,7 +45,7 @@ module.exports = {
     .setName('top-3')
     .setDescription('Show top 3 scholars'),
   async execute(interaction) {
-    if (interaction.member.roles.cache.has('863179537324048414')) return;
+    if (interaction.user.id !== interaction.guild.ownerId) return;
     await getTop3(interaction);
   },
 };
