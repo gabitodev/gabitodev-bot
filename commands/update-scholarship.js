5aec1e19ebea4ea6679bbaee2afc9bfde5520f30
gabitodev <gabitodev@gmail.com>
2022-02-17 10:24:22 -0400

    better error handling, prevented sql injection

--- commands/update-scholarship.js
+++ commands/update-scholarship.js
@@ -5,30 +5,42 @@ const { calcTeamStats } = require('../modules/team-stats');
 const { updateScholar } = require('../modules/database-querys');
 
 const getScholars = async () => {
-  const text = `
-  SELECT * FROM teams
-  INNER JOIN scholars 
-  ON scholars.discord_id = teams.discord_id
-  ORDER BY teams.team_id`;
-  const scholars = await many(text);
-  return scholars;
+  try {
+    const scholars = await many({
+      text: `
+      SELECT * FROM teams
+      INNER JOIN scholars 
+      ON scholars.discord_id = teams.discord_id
+      ORDER BY teams.team_id
+      `,
+    });
+    return scholars;
+  } catch (error) {
+    return null;
+  }
+
 };
 
 const updateScholarship = async (interaction) => {
-  await interaction.reply('Updating database...');
+  await interaction.reply('Updating scholarship...');
+
   // 1. We obtain all the shcolars
   const scholars = await getScholars();
+  if (!scholars) return await interaction.editReply('Failed to update the scholarship because you dont have scholars or teams.');
+
   // 2. We obtain each ronin address
   const roninsAddresses = scholars.map(({ teamAddress }) => teamAddress);
+
   // 3. We obtain the ronin data
   const roninData = await getRoninData(roninsAddresses);
+
   // 4. We update each scholar to the database
   for (const scholarData of scholars) {
     const teamStats = calcTeamStats(scholarData, roninData);
     updateScholar(teamStats);
   }
   // 5. We display the response to the user
-  await interaction.editReply('The database has been updated!');
+  await interaction.editReply('The scholarship has been updated.');
 };
 
 module.exports = {
@@ -36,7 +48,7 @@ module.exports = {
     .setName('update-scholarship')
     .setDescription('Updates the scholarship'),
   async execute(interaction) {
-    if (interaction.member.roles.cache.has('863179537324048414')) return;
+    if (interaction.user.id !== interaction.guild.ownerId) return;
     await updateScholarship(interaction);
   },
 };
