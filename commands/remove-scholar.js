f1b53602a1eab79fbc2d0759414960086e8ec9a6
gabitodev <gabitodev@gmail.com>
2022-02-17 08:28:12 -0400

    command name changed, prevented sql injection, better error handling

---
+++ commands/remove-scholar.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { result } = require('../database');
+
+const removeScholar = async (interaction) => {
+  // 1. We define the variables
+  const discordId = interaction.options.getUser('discord-user').id;
+  const member = await interaction.guild.members.fetch(discordId);
+
+  // 2. We remove the scholar from the database and kick the user
+  const { rowCount } = await result({
+    text: 'DELETE FROM scholars WHERE discord_id = $1',
+    values: [discordId],
+  });
+  if (rowCount === 0) return await interaction.reply('Could not be remvoved because the discord user is not a scholar.');
+  member.kick('He will no longer be part of the scholarship');
+
+  // 3. Display the response to the user
+  await interaction.reply(`Successfully removed and kicked the scholar <@${discordId}>`);
+};
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('remove-scholar')
+    .setDescription('Removes and kicks a scholar')
+    .addUserOption(option =>
+      option
+        .setName('discord-user')
+        .setDescription('Discord user to remove')
+        .setRequired(true)),
+  async execute(interaction) {
+    if (interaction.user.id !== interaction.guild.ownerId) return;
+    await removeScholar(interaction);
+  },
+};
