66659ead98806dfc204e7f0fc9836624c2196f60
gabitodev <gabitodev@gmail.com>
2022-02-17 06:27:53 -0400

    new function to get the days to the next claim

--- modules/utils-date.js
+++ modules/utils-date.js
@@ -12,4 +12,11 @@ module.exports = {
     const date = (DateTime.fromSeconds(unixDate)).toISODate();
     return date;
   },
+  daysToNextClaim(lastClaim) {
+    const now = DateTime.now();
+    const lastClaimDate = DateTime.fromISO(lastClaim);
+    const difference = Interval.fromDateTimes(now, lastClaimDate);
+    const days = difference.length('days');
+    return `${days.toFixed(0)} days`;
+  },
 };
