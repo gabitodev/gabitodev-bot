b2258be2ec59006f13740a74332f3ec5b60834ab
gabitodev <gabitodev@gmail.com>
2022-02-21 12:53:59 -0400

    try catch added

--- modules/update-scholarship.js
+++ modules/update-scholarship.js
@@ -22,20 +22,25 @@ const getScholars = async () => {
 
 module.exports = {
   async updateScholarship() {
-    // 1. We obtain all the shcolars
-    const scholars = await getScholars();
+    try {
+      // 1. We obtain all the shcolars
+      const scholars = await getScholars();
 
-    // 2. We obtain each ronin address
-    const roninsAddresses = scholars.map(({ teamAddress }) => teamAddress);
+      // 2. We obtain each ronin address
+      const roninsAddresses = scholars.map(({ teamAddress }) => teamAddress);
 
-    // 3. We obtain the ronin data
-    const roninData = await getRoninData(roninsAddresses);
+      // 3. We obtain the ronin data
+      const roninData = await getRoninData(roninsAddresses);
 
-    // 4. We update each scholar to the database
-    for (const scholarData of scholars) {
-      const teamStats = calcTeamStats(scholarData, roninData);
-      updateScholar(teamStats);
+      // 4. We update each scholar to the database
+      for (const scholarData of scholars) {
+        const teamStats = calcTeamStats(scholarData, roninData);
+        updateScholar(teamStats);
+      }
+      return 'success';
+    } catch (error) {
+      console.log(error);
+      return 'failed';
     }
-    return 'success';
   },
 };
