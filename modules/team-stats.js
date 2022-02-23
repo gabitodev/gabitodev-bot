d2b26635939a3ce779c40e38f42dac36cd1aa9ec
gabitodev <gabitodev@gmail.com>
2022-02-23 11:31:27 -0400

    changes in how is calculated the team stats

--- modules/team-stats.js
+++ modules/team-stats.js
@@ -1,7 +1,11 @@
 const { DateTime, Interval } = require('luxon');
-const { formatDate } = require('./utils-date');
 
-const calcDaysSinceLastClaim = (date) => {
+const getIsoDateFromSeconds = (unixDate) => {
+  const date = (DateTime.fromSeconds(unixDate)).toISODate();
+  return date;
+};
+
+const getDaysSinceLastClaim = (date) => {
   const now = DateTime.now();
   const lastClaimDate = DateTime.fromISO(date);
   const difference = Interval.fromDateTimes(lastClaimDate, now);
@@ -9,47 +13,34 @@ const calcDaysSinceLastClaim = (date) => {
   return days;
 };
 
-const calcScholarFee = (date, freeDays, dailyFee, inGameSlp, mmr) => {
+const calcScholarFee = (daysSinceLastClaim, freeDays, dailyFee, inGameSlp) => {
   if (dailyFee > 1) {
-    const daysSinceLastClaim = calcDaysSinceLastClaim(date);
-    let fee = dailyFee * (daysSinceLastClaim - freeDays);
-    fee = Math.max(0, fee);
-    return fee;
-  } else if (mmr < 1300 && dailyFee === 0) {
-    return inGameSlp * 0.60;
-  } else if (mmr < 1700 && dailyFee === 0) {
-    return inGameSlp * 0.50;
-  } else if (mmr < 2200 && dailyFee === 0) {
-    return inGameSlp * 0.40;
+    const fee = dailyFee * (daysSinceLastClaim - freeDays);
+    const feeRounded = Math.max(0, fee);
+    return feeRounded;
   } else {
     return inGameSlp * dailyFee;
   }
 };
 
-const calcScholarSLP = (total, manager) => {
-  const scholarSLP = total - manager;
-  return scholarSLP;
-};
-
-const calcAverageSLP = (slp, date) => {
-  const daysSinceLastClaim = calcDaysSinceLastClaim(date);
+const calcAverageSLP = (slp, daysSinceLastClaim) => {
   if (daysSinceLastClaim === 0) {
-    const average = 0;
-    return average;
+    return 0;
   } else {
-    const average = (slp / daysSinceLastClaim);
-    return Math.round(average);
+    const average = Math.round(slp / daysSinceLastClaim);
+    return average;
   }
 };
 
-const calcTeamStats = (teamData, roninData) => {
+const getTeamSummary = (teamData, roninData) => {
   const { freeDays, teamId, dailyFee, teamAddress, yesterdaySlp } = teamData;
   const { lastClaim: lastClaimUnix, nextClaim: nextClaimUnix, inGameSlp, mmr } = roninData[teamAddress];
-  const lastClaim = formatDate(lastClaimUnix);
-  const nextClaim = formatDate(nextClaimUnix);
-  const managerSlp = calcScholarFee(lastClaim, freeDays, dailyFee, inGameSlp, mmr);
-  const scholarSlp = calcScholarSLP(inGameSlp, managerSlp);
-  const averageSlp = calcAverageSLP(inGameSlp, lastClaim);
+  const lastClaim = getIsoDateFromSeconds(lastClaimUnix);
+  const nextClaim = getIsoDateFromSeconds(nextClaimUnix);
+  const daysSinceLastClaim = getDaysSinceLastClaim(lastClaim);
+  const managerSlp = calcScholarFee(daysSinceLastClaim, freeDays, dailyFee, inGameSlp);
+  const averageSlp = calcAverageSLP(inGameSlp, daysSinceLastClaim);
+  const scholarSlp = inGameSlp - managerSlp;
   const todaySlp = inGameSlp - yesterdaySlp;
   const teamStats = {
     teamId,
@@ -65,4 +56,4 @@ const calcTeamStats = (teamData, roninData) => {
   return teamStats;
 };
 
-module.exports.calcTeamStats = calcTeamStats;
+module.exports.calcTeamStats = getTeamSummary;
