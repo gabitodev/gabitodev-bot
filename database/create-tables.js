61bf404f95e4f8c1bd8070d4132892e02afb1d6d
gabitodev <gabitodev@gmail.com>
2022-02-27 18:54:11 -0400

    dabatase changed from postgres to sqlite

---
+++ database/create-tables.js
@@ -0,0 +1,65 @@
+import { db } from './index.js';
+
+const createScholarsTable = async () => {
+  db.prepare(`
+  CREATE TABLE IF NOT EXISTS scholars (
+    discord_id TEXT PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT (strftime('%s', 'now')),
+    updated_at TIMESTAMP NOT NULL DEFAULT (strftime('%s', 'now')),
+    full_name TEXT,
+    payout_address TEXT
+  );
+  `).run();
+  console.log('Scholars table created');
+};
+
+const createTeamsTable = async () => {
+  db.prepare(`
+  CREATE TABLE IF NOT EXISTS teams (
+    team_id INTEGER PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT (strftime('%s', 'now')),
+    updated_at TIMESTAMP NOT NULL DEFAULT (strftime('%s', 'now')),
+    discord_id TEXT REFERENCES scholars(discord_id) ON DELETE SET NULL,
+    team_address TEXT UNIQUE NOT NULL,
+    daily_fee INTEGER DEFAULT 0,
+    free_days INTEGER DEFAULT 0,
+    last_claim DATE,
+    next_claim DATE,
+    in_game_slp INTEGER DEFAULT 0,
+    manager_slp INTEGER DEFAULT 0,
+    scholar_slp INTEGER DEFAULT 0,
+    mmr INTEGER DEFAULT 1200,
+    average_slp INTEGER DEFAULT 0,
+    today_slp INTEGER DEFAULT 0,
+    yesterday_slp INTEGER DEFAULT 0
+  );
+  `).run();
+  console.log('Teams table created');
+};
+
+const createTrigger = async () => {
+  const tables = ['scholars', 'teams'];
+  for (const tableName of tables) {
+    db.prepare(`
+      CREATE TRIGGER set_timestamp_${tableName}
+      BEFORE UPDATE ON ${tableName}
+      FOR EACH ROW
+      BEGIN
+        UPDATE ${tableName} SET updated_at = (strftime('%s', 'now'));
+      END;
+    `).run();
+  }
+  console.log('Triggers created');
+};
+
+(async () => {
+  try {
+    console.log('Creating tables...');
+    await createScholarsTable();
+    await createTeamsTable();
+    await createTrigger();
+    console.log('Sucessfully created tables!');
+  } catch (error) {
+    console.log(error);
+  }
+})();
--- database/index.js
+++ database/index.js
@@ -1,31 +1,4 @@
-import pgPromise from 'pg-promise';
+import Database from 'better-sqlite3';
+const MAIN_DB = 'gabitoAxieBot.db';
 
-const pgp = pgPromise({
-  receive: (data) => {
-    camelizeColumns(data);
-  },
-});
-
-const camelizeColumns = (data) => {
-  const tmp = data[0];
-  for (const prop in tmp) {
-    const camel = pgp.utils.camelize(prop);
-    if (!(camel in tmp)) {
-      for (let i = 0; i < data.length; i++) {
-        const d = data[i];
-        d[camel] = d[prop];
-        delete d[prop];
-      }
-    }
-  }
-};
-
-const config = {
-  host: process.env.HOST_DB,
-  database: process.env.DATA_DB,
-  user: process.env.USER_DB,
-  password: process.env.PASS_DB,
-  max: 30,
-};
-
-export const db = pgp(config);
+export const db = new Database(MAIN_DB);
