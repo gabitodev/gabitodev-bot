88e24ef31ad3b6dd551a9736cf1e324324993353
gabitodev <gabitodev@gmail.com>
2022-02-27 21:06:02 -0400

    sqlite ready!

--- modules/update-scholar.js
+++ modules/update-scholar.js
@@ -1,11 +1,9 @@
 import { db } from '../database/index.js';
 
-export const updateScholar = async ({ lastClaim, nextClaim, inGameSlp, managerSlp, scholarSlp, mmr, averageSlp, todaySlp, teamId }) => {
-  await db.none({
-    text: `
-    UPDATE teams
-    SET last_claim = $1, next_claim = $2, in_game_slp = $3, manager_slp = $4, scholar_slp = $5, mmr = $6, average_slp = $7, today_slp = $8
-    WHERE team_id = $9`,
-    values: [lastClaim, nextClaim, inGameSlp, managerSlp, scholarSlp, mmr, averageSlp, todaySlp, teamId],
-  });
+export const updateScholar = ({ lastClaim, nextClaim, inGameSlp, managerSlp, scholarSlp, mmr, averageSlp, todaySlp, teamId }) => {
+  db.prepare(`
+  UPDATE teams
+    SET last_claim = ?, next_claim = ?, in_game_slp = ?, manager_slp = ?, scholar_slp = ?, mmr = ?, average_slp = ?, today_slp = ?
+    WHERE team_id = ?
+  `).run(lastClaim, nextClaim, inGameSlp, managerSlp, scholarSlp, mmr, averageSlp, todaySlp, teamId);
 };
