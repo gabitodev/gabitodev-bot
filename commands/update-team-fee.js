dc6a51153db00fb58770ff522239c4a5dbdaf966
gabitodev <gabitodev@gmail.com>
2022-04-06 20:40:26 -0400

    function to close db added

--- commands/update-team-fee.js
+++ commands/update-team-fee.js
@@ -8,6 +8,8 @@ const updateTeamFee = async (interaction) => {
 
   // 2. We add/remove the 20 energies to the scholar
   const { changes } = db.prepare('UPDATE teams SET daily_fee = ? WHERE team_id = ?').run(dailyFee, teamId);
+  db.close();
+
   if (changes === 0) return await interaction.reply('The team could not be updated because it does not exist in the database.');
 
   // 3. Display the response to the user
