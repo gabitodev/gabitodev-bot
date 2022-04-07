aceaf239437dd4935e7a085d737a99bef1bfc2b7
gabitodev <gabitodev@gmail.com>
2022-04-06 20:25:30 -0400

    const name changed to match db name, close db function added

--- commands/add-scholar.js
+++ commands/add-scholar.js
@@ -15,19 +15,20 @@ const addScholar = async (interaction) => {
     // 1. We define the variables
     const discordId = interaction.options.getUser('discord-user').id;
     const name = interaction.options.getString('name');
-    const roninAddress = interaction.options.getString('ronin-address');
+    const payoutAddress = interaction.options.getString('payout-address');
     const member = await interaction.guild.members.fetch(discordId);
     const role = await interaction.guild.roles.fetch(process.env.SCHOLAR_ROLE_ID);
 
     // Check if role is correct
     if (!role) return await interaction.reply('Wrong role! Make sure it is correct.');
     // Check is ronin is valid and use the ronin prefix
-    if (validateRoninAddress(roninAddress) === false) {
-      return await interaction.reply(`Wrong address! Make sure it starts with the ${inlineCode('ronin:')} prefix and is complete.`);
+    if (validateRoninAddress(payoutAddress) === false) {
+      return await interaction.reply(`Wrong address! Make sure it starts with the ${inlineCode('ronin:')} prefix and is 46 characters long.`);
     }
 
     // 2. We create the scholar in the database and add Scholar Role
-    db.prepare('INSERT INTO scholars (discord_id, full_name, ronin_address) VALUES (?, ?, ?)').run(discordId, name, roninAddress);
+    db.prepare('INSERT INTO scholars (discord_id, full_name, ronin_address) VALUES (?, ?, ?)').run(discordId, name, payoutAddress);
+    db.close();
     member.roles.add(role);
 
     // 3. Display the response to the user
@@ -36,7 +37,7 @@ const addScholar = async (interaction) => {
       ${bold('Successfully created a new scholar!')}
       User: <@${discordId}>
       Name: ${inlineCode(name)}
-      Ronin Address: ${inlineCode(roninAddress)}
+      Payout Address: ${inlineCode(payoutAddress)}
       Role: <@&${process.env.SCHOLAR_ROLE_ID}>`,
     });
   } catch (error) {
@@ -65,8 +66,8 @@ export const command = {
         .setRequired(true))
     .addStringOption(option =>
       option
-        .setName('ronin-address')
-        .setDescription('Scholar ronin address')
+        .setName('payout-address')
+        .setDescription('Scholar payout address')
         .setRequired(false)),
   async execute(interaction) {
     if (interaction.user.id !== interaction.guild.ownerId) return;
